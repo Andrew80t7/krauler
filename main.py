8e98efbccc50e243a4fc703a8b288a7e0069529a
Andrew Sultanov <vysotnik3@gmail.com>
2025-03-25 22:42:35 +0500

    Реализован рекурсивный поиск

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 import asyncio
 from telegram_api.client import get_client
 from telegram_api.search import search_channels
-from telegram_api.downloader import process_channel
+from recursive_search import process_channel_recursive
 from config import load_config
 from logger import setup_logging
 from db import init_db
 
 
 async def main_async():
+    try:
+        setup_logging()
+        config = load_config('config.json')
+        init_db()
+        client = await get_client(config)
 
-    setup_logging()
-    config = load_config('config.json')
-    init_db()
+        keyword = "tech"
+        channels = await search_channels(client, keyword, limit=500)
 
-    client = await get_client(config)
+        if not channels:
+            print("Каналы не найдены")
+            return
 
-    keyword = "tech"
-    channels = await search_channels(client, keyword, limit=500)
+        semaphore = asyncio.Semaphore(5)
 
-    for channel in channels:
-        print(channel.username)
-        asyncio.create_task(process_channel(client, channel.username))
+        async def process_channel_recursive_with_semaphore(client, username):
+            async with semaphore:
+                await process_channel_recursive(client, username)
 
-    await client.run_until_disconnected()
+        tasks = []
+        for channel in channels:
+            print(f"Начинаю обработку канала: {channel.username}")
+            task = asyncio.create_task(process_channel_recursive_with_semaphore(client, channel.username))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+        await client.disconnect()
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 
 if __name__ == "__main__":
